b45d4abaa69df3eb1634e4009ace38fb368ebfca
robert <robert@Adelphia>
2017-05-04 01:01:15 -0400

    Blank page for resources module

---
+++ assets/modules/resources/module.js
@@ -0,0 +1,15 @@
+
+
+iLoveLAMP.modules.resources = (function(){
+	
+	function init(){
+		
+	}
+	
+	return {
+		requiresServer: true,
+		title: "Resources",
+		icon: "bar-chart",
+		init: init
+	};
+})();
